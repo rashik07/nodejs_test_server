8a505390d5b4c956e80e6bb6b3894dc5ec99f058
rashik07 <saif67090707@gmail.com>
2023-10-06 00:36:02 +0600

    vercel update

--- middileware/uploader.js
+++ middileware/uploader.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "public/uploads/",
+  destination: "uploads/",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + file.originalname);
